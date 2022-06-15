1bcf63d0dbbee17137dcd7292efeaebaf71fbef7
RhoNit <tb.edumax29@gmail.com>
2022-06-15 10:35:33 +0530

    subset sum problem (dp+memoization/top down approach)

---
+++ 0-1Knapsack/subset-sum.cpp
@@ -0,0 +1,21 @@
+class Solution{   
+public:
+    int dp[100][100*100];
+    int helper(vector<int> &arr, int i, int currSum, int targetSum) {
+        //base case
+        if(currSum == targetSum) { return 1; }
+        if(i == arr.size() and currSum != targetSum) { return 0; }
+        
+        //checking if it is already present there in dp array
+        if(dp[i][currSum] != -1) { return dp[i][currSum]; }
+        
+        //storing in dp array + recursive call
+        dp[i][currSum] = (helper(arr, i+1, currSum+arr[i], targetSum) || helper(arr, i+1, currSum, targetSum));
+        return dp[i][currSum];
+    }
+    
+    bool isSubsetSum(vector<int>arr, int sum){
+        memset(dp, -1, sizeof(dp));
+        return helper(arr, 0, 0, sum);
+    }
+};
